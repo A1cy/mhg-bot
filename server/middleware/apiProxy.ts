f530817567c5b16734fd22057d03964c93722f7d
A1cy <a1hvdy@gmail.com>
2024-03-10 16:38:43 +0300

    a

--- server/middleware/apiProxy.ts
+++ server/middleware/apiProxy.ts
@@ -1,15 +1,15 @@
-import { createProxyMiddleware } from 'http-proxy-middleware'
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 export default defineEventHandler(async (event) => {
-    await new Promise((resolve, reject) => {
-        createProxyMiddleware({
-            target: process.env.SERVER_DOMAIN,
-            changeOrigin: true, // This might be necessary for Vercel deployments
-            pathRewrite: {
-                '^/api/': '', // assuming you want to strip '/api/' from the proxied request
-            },
-        })(event.node.req, event.node.res, (err) => {
-            if (err) reject(err);
-            else resolve(true);
-        });
+    createProxyMiddleware({
+        target: 'https://mhg-backend.onrender.com', // The actual target server
+        changeOrigin: true,
+        pathRewrite: {
+            '^/api/': '/api/', // You might need to adjust the rewrite rules based on your setup
+        },
+    })(event.req, event.res, (error) => {
+        if (error) {
+            console.error('Proxy error:', error);
+        }
     });
 });
